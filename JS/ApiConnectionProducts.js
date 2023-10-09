05160d5c676f4fed2f3c6eacf047ebd0e8a7ed9b
lucas Cid <lucasfelipaa@gmail.com>
2023-10-09 10:25:35 -0300

    addiction of functionality of add items

--- JS/ApiConnectionProducts.js
+++ JS/ApiConnectionProducts.js
@@ -1,41 +1,55 @@
 export class DataProducts{
+    produtos = document.querySelectorAll('.product-card')
+    secao = document.querySelector('.secao-produtos')
+    constructor(){/**remover os produtos */
+        this.produtos.forEach((produto)=>{
+            produto.remove()
+        })
+    }
+
     DataProd = []
     secctionProducts = document.querySelector('.secao-produtos')
     load(){
         
     }
-    update(){
-        
+    update(elemento){
+        this.secao.appendChild(elemento)
     }
-    createSection(){
+    createSection(product){
        const section = `
-        <img class="product-img" src="" alt="">
-        <div class="description">
-            <h1 id="product-title">bone</h1>
-            <p id="price">price</p>
-
-        </div>
+       <img class="product-img" src="${product.image}" alt="">
+       <div class="description">
+           <h1 id="product-title">${product.title}</h1>
+           <p id="price">${product.price}</p>
+           <p id="description">${product.description}</p>
+           <p id="rate">${product.rating.rate}</p>
+       </div>
        
        `
         const element = document.createElement('div')
         element.className = 'product-card'
         element.innerHTML = section
-
+        return element
     }
 }
 
 export class ApiConnectionProducts extends DataProducts { 
 
+    constructor(){
+        super()
+    }
     getProducts(id){
         console.log('chegou no getproducts')
         const endpoint = `https://fakestoreapi.com/products/${id}` 
         return fetch(endpoint)
         .then(data => data.json())
-        .then(({price, id, image, title})=>({
+        .then(({price, id, image, title,rating,description})=>({
             price,
             id,
             image,
-            title
+            title,
+            rating,
+            description
         }))
     }
 
@@ -43,7 +57,8 @@ export class ApiConnectionProducts extends DataProducts {
         const product = await this.getProducts(id);
         console.log(product);
         this.DataProd.push(product)
-        this.createSection(product)
+        const elemento = this.createSection(product)
+        this.update(elemento)
         return product;
     }
 }
--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,13 @@
 import { ApiConnectionProducts } from "./ApiConnectionProducts.js"
 const app = new ApiConnectionProducts()
 console.log('oi')
+const addItemButton = document.querySelector("#add-product")
+
+addItemButton.addEventListener('click',()=>{
+    
+})
 app.add(2)
 app.add(5)
 app.add(3)
+app.add(13)
+app.add(15)
